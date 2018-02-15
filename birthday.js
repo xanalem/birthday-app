5c962dbf9b122e01a0d6fe206bde4bc76728404b
Brandon S Navone <bn03907@srcs.loc>
2018-02-15 12:03:41 -0800

    purge of stupid diplay sign function

--- birthday.js
+++ birthday.js
@@ -24,29 +24,29 @@ function getSign() {
     console.log(birth);
 
     if ((month == 1 && day <= 20) || (month == 12 && day >= 22)) {
-        return "Capricorn";
+        return "Your sign is Capricorn";
     } else if ((month == 1 && day >= 21) || (month == 2 && day <= 18)) {
-        return "Aquarius";
+        return "Your sign is Aquarius";
     } else if ((month == 2 && day >= 19) || (month == 3 && day <= 20)) {
-        return "Pisces";
+        return "Your sign is Pisces";
     } else if ((month == 3 && day >= 21) || (month == 4 && day <= 20)) {
-        return "Aries";
+        return "Your sign is Aries";
     } else if ((month == 4 && day >= 21) || (month == 5 && day <= 20)) {
-        return "Taurus";
+        return "Your sign is Taurus";
     } else if ((month == 5 && day >= 21) || (month == 6 && day <= 20)) {
-        return "Gemini";
+        return "Your sign is Gemini";
     } else if ((month == 6 && day >= 22) || (month == 7 && day <= 22)) {
-        return "Cancer";
+        return "Your sign is Cancer";
     } else if ((month == 7 && day >= 23) || (month == 8 && day <= 23)) {
-        return "Leo";
+        return "Your sign is Leo";
     } else if ((month == 8 && day >= 24) || (month == 9 && day <= 23)) {
-        return "Virgo";
+        return "Your sign is Virgo";
     } else if ((month == 9 && day >= 24) || (month == 10 && day <= 23)) {
-        return "Libra";
+        return "Your sign is Libra";
     } else if ((month == 10 && day >= 24) || (month == 11 && day <= 22)) {
-        return "Scorpio";
+        return "Your sign is Scorpio";
     } else if ((month == 11 && day >= 23) || (month == 12 && day <= 21)) {
-        return "Sagittarius";
+        return "Your sign is Sagittarius";
     }
 }
 
